ac811458337b76e894db105dd063ba630689d4be
Shubham singh <shubham1041singh@gmail.com>
2024-07-24 18:36:59 +0530

    3 day completed

--- 3day.js
+++ 3day.js
@@ -74,7 +74,15 @@ switch(day)
 let num=10;
 console.log((num%2==0) ? 'positive' : 'negative')
 
-
+// task 7
+let year = 2000;
+if(((year%4==0) && (year%100!=0))  || (year%400==0))
+{
+    console.log("year is a leap year");
+}
+else{
+    console.log("year is not a leap year");
+}
 
 
 

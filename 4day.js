f9bcfa1894edf8fa554791a0cfaaf037ebad0f9e
Shubham singh <shubham1041singh@gmail.com>
2024-07-24 21:52:10 +0530

    day4.js

---
+++ 4day.js
@@ -0,0 +1,34 @@
+// task 1
+ for (let index = 1; index <=10; index++) {
+     console.log(index);  
+ }
+// task 2
+ const a =5;
+ for (let index = 1; index <=10; index++) {
+    console.log(index*a);  
+}
+// task 3
+sum =0;
+i=0;
+while( i < 10)
+{
+    sum = sum + i;
+    i++;
+}
+console.log(sum);
+// task 4
+i=10;
+while (i>0)
+{
+    console.log(i);
+    i--;
+}
+// task 7
+for( let j=0; j<=5 ;j++)
+{
+    for( let k=0; k<=5 ;k++)
+    {
+        console.log('*');
+    }
+    
+}

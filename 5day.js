af1dd2fbdb82e4c5488b8f02dd3b03deb32b2f52
Shubham singh <shubham1041singh@gmail.com>
2024-07-25 16:30:22 +0530

    day5 done

--- 5day.js
+++ 5day.js
@@ -32,3 +32,57 @@ function max(num1 , num2)
 }
 max(10,20)
 // task 4
+function concatenate(str1 , str2)
+{
+  return str1 + str2
+}
+const result = concatenate( "hello"  ,  "shubham")
+console.log(result)
+// task 5
+let sum = (a,b) =>
+{
+    let c = a*b
+    console.log(c)
+}
+sum(2,3)
+// task 6
+let containChar = (str , char) =>  str.includes(char)
+
+console.log(containChar("hello" , "e"));
+// task 7
+function prod(a , b=1 )
+{
+     c=a*b;
+     console.log(c);
+}
+prod(4,6)
+// task 8
+function greetings(name , age)
+{
+   console.log(`hello ${name} , your age is ${age}`);
+}
+greetings("shubham" , 20)
+// task 9
+const repeatFunction = (func , times) => {
+    for(let i=0;i<times;i++)
+    {
+        func()
+    }
+}
+sayHello = () =>{
+    console.log("hello");
+}
+repeatFunction(sayHello , 2)
+// task 10
+const twoFunction = (func , funct ,  value) =>
+{
+
+}
+ 
+
+
+
+
+
+
+

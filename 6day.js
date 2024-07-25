0d2285f46357803d052b7ac0796a9b2524b75d9d
Shubham singh <shubham1041singh@gmail.com>
2024-07-26 05:20:58 +0530

    6 day started

---
+++ 6day.js
@@ -0,0 +1,27 @@
+// task 1
+let a= [ 1,2,3,4,5 ];
+for(let i=0;i<5;i++)
+{
+    console.log(a[i])
+}
+// task 2
+console.log(a[0])
+console.log(a[4])
+// task 3
+a.push(3)
+for(let i=0;i<6;i++)
+    {
+        console.log(a[i])
+    }
+// task 4
+a.pop()
+for(let i=0;i<6;i++)
+    {
+        console.log(a[i])
+    }
+// task 5
+a.shift(99)   
+for(let i=0;i<6;i++)
+    {
+        console.log(a[i])
+    }

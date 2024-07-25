cf01f830a9c38aafc078a2921963339a5eb57f16
Shubham singh <shubham1041singh@gmail.com>
2024-07-25 15:48:09 +0530

    day5

---
+++ 5day.js
@@ -0,0 +1,34 @@
+// task 1
+function EvenOrOdd(num)
+{
+    if(num%2==0)
+    {
+        console.log("number is even");
+    }
+    else 
+    {
+        console.log("number is odd");
+    }
+}
+EvenOrOdd(102);
+// task 2
+function Square(num)
+{
+    let a = num*num;
+    console.log(a);
+}
+Square(10)
+// task 3
+function max(num1 , num2)
+{
+    if(num1>num2)
+    {
+        console.log("num1 is greater than num2");
+    }
+    else
+    {
+        console.log("num2 is greater than num1");
+    }
+}
+max(10,20)
+// task 4

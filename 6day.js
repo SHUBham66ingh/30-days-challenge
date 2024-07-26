35514727f5c9660155f0966ef27b9ff7d5ab8c7f
Shubham singh <shubham1041singh@gmail.com>
2024-07-26 05:56:27 +0530

    6 day done

--- 6day.js
+++ 6day.js
@@ -20,8 +20,41 @@ for(let i=0;i<6;i++)
         console.log(a[i])
     }
 // task 5
-a.shift(99)   
+a.shift()   
 for(let i=0;i<6;i++)
     {
         console.log(a[i])
     }
+// task 6
+a.unshift(99)  
+for(let i=0;i<6;i++)
+    {
+        console.log(a[i])
+    }  
+// task 7
+let b = [2,3,4,5,6];
+ let doubleArray = b.map(i=>i*2);
+ console.log(doubleArray)
+// task 8
+let c = [2,3,4,5,6];
+let EvenArray = c.filter(i=> i%2 ==0);
+console.log(EvenArray)
+// task 9
+let d = [2,3,4,5,6];
+let Sum = d.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log(Sum)
+// task 10
+let l = [ 1,2,3,4,5 ];
+for(let i=0;i<5;i++)
+{
+    console.log(l[i])
+}
+// task 11
+array.forEach(element => {
+    
+});
+// task 12
+
+
+
+
